2a68f3eee4a4cb0111044d7f3fa97ccac33e183c
AaronDLP <aaron@doublelinepartners.com>
2016-10-07 14:17:02 -0500

    fix casing issue

--- Database/Security/AuthorizationHierarchy.sql
+++ Database/Security/AuthorizationHierarchy.sql
@@ -1,6 +1,6 @@
 ﻿CREATE SECURITY POLICY [Security].[AuthorizationHierarchy]
-    ADD FILTER PREDICATE [Security].[fn_securityPredicate]([Uid]) ON [dbo].[People],
-    ADD FILTER PREDICATE [Security].[fn_securityPredicate]([Uid]) ON [dbo].[Schools],
-    ADD FILTER PREDICATE [Security].[fn_securityPredicate]([Uid]) ON [dbo].[Districts]
+    ADD FILTER PREDICATE [Security].[fn_securityPredicate]([uid]) ON [dbo].[People],
+    ADD FILTER PREDICATE [Security].[fn_securityPredicate]([uid]) ON [dbo].[Schools],
+    ADD FILTER PREDICATE [Security].[fn_securityPredicate]([uid]) ON [dbo].[Districts]
     WITH (STATE = OFF);
 
